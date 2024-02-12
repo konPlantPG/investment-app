819581a0d5f62675060b2bd05b2b49f1580da565
konPlantPG <hatasagoki@gmail.com>
2024-02-13 01:03:17 +0900

    表の作成

--- pages/components/RankingTemplate.jsx
+++ pages/components/RankingTemplate.jsx
@@ -1,9 +1,24 @@
+import React from 'react'
 
-const RankingTemplate = (props) =>{
-    return(
-        <>
-            {props.posts}
-        </>
+const RankingTemplate = (props) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.posts.map((post, postIndex) => (
+                    <tr key={postIndex}> 
+                        <td>{post.name}</td>
+                        <td>{post.price}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
+
 export default RankingTemplate
--- pages/ranking-page.js
+++ pages/ranking-page.js
@@ -5,12 +5,44 @@ import { listPOSTS } from "@/src/graphql/queries"
 
 Auth.currentAuthenticatedUser()
   .then(user => console.log(user))
-  .catch(err => console.log('ユーザーはサインインしていません', err));
+  .catch(err => console.log('ユーザーはサインインしていません', err))
+
+const dummyDatas = 
+  {
+    "data": {
+      "listPOSTS": {
+        "items": [
+          {
+            "id": "31aeee20-3635-4f05-b4c9-74c0daaaef44",
+            "name": "test1",
+            "price": 150
+          },
+          {
+            "id": "156c9104-50f7-4869-9844-55b55858ca5f",
+            "name": "test4",
+            "price": 250
+          },
+          {
+            "id": "b3376332-3ccf-4741-995e-0b429ab17823",
+            "name": "test2",
+            "price": 300
+          },
+          {
+            "id": "3c716a01-8ad5-4e53-b466-e55794aede3f",
+            "name": "test3",
+            "price": 150
+          }
+        ]
+      }
+    }
+  }
+
 
 const RankingPage= () => {
   const[ posts, setPosts ] = useState([])
 
   const fetchPosts = () => {
+    
     API.graphql({
       query: listPOSTS
     })
@@ -19,11 +51,10 @@ const RankingPage= () => {
         setPosts(postsList)
       })
       .catch(error => {
+        setPosts(dummyDatas.data.listPOSTS.items)
         console.error('エラー発生:', error)
       })
   }
-
-
   useEffect(() =>{
     fetchPosts()
   },[])
