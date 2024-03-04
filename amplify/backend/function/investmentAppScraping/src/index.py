4c7cc90cd3a1557a2c413aa12e8e384c4244d594
konPlantPG <hatasagoki@gmail.com>
2024-03-04 16:17:45 +0900

    データのアップロード

--- amplify/backend/function/investmentAppScraping/src/index.py
+++ amplify/backend/function/investmentAppScraping/src/index.py
@@ -1,18 +1,17 @@
-# 変更する
+import os
 import json
+import requests
 from stockData import collect_stock_datas
 
+API_URL = os.getenv("APPSYNC_URL")
+API_KEY = os.getenv("APPSYNC_API_KEY")
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  collect_stock_datas.test()   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    print('received event:')
+    print(event)
+
+    headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': API_KEY
+    }
+    stock_info = collect_stock_datas.collect()
--- amplify/backend/function/investmentAppScraping/src/stockData/collect_stock_datas.py
+++ amplify/backend/function/investmentAppScraping/src/stockData/collect_stock_datas.py
@@ -4,9 +4,14 @@ import pandas as pd
 from yahooquery import Ticker
 from tqdm import tqdm
 import time
+import requests
+import json
+import uuid
 
-def test():
-    # 東証上場銘柄一覧を読み込む
+API_URL = os.getenv("API_INVESTMENTAPP_GRAPHQLAPIENDPOINTOUTPUT")
+API_KEY = os.getenv("API_INVESTMENTAPP_GRAPHQLAPIKEYOUTPUT")
+
+def collect():
     if not os.path.isfile('/var/task/stockData/data_j.xls'):
         print(os.getcwd())
         print('東証上場銘柄一覧を[data_j.xls]のファイル名で保存してください。')
@@ -15,47 +20,68 @@ def test():
     print('東証上場銘柄一覧を読み込みます。')
     df_data_j = pd.read_excel('/var/task/stockData/data_j.xls', index_col=None)
 
-    # 特定の条件に一致する行を除外
     df_data_j = df_data_j[df_data_j['市場・商品区分'] != 'REIT・ベンチャーファンド・カントリーファンド・インフラファンド']
     df_data_j = df_data_j[df_data_j['市場・商品区分'] != 'ETF・ETN']
     df_data_j = df_data_j[df_data_j['コード'] != 25935]
 
-    # 出力用のデータフレームを用意
+    
     df_stock_info = pd.DataFrame()
-
-    # コードリストの長さを取得
     total_codes = len(df_data_j['コード'])
+    start_index = 0
+    end_index = int(np.ceil(total_codes * 0.01))
 
-    # 処理する範囲を計算（ここでは0~10%）
-    start_index = 0  # 開始インデックス
-    end_index = int(np.ceil(total_codes * 0.1))
-
-    # tqdmを使用して進捗を表示しながら、指定された範囲のコードを処理
     for ticker in tqdm(df_data_j['コード'][start_index:end_index]):
         ticker_num = str(ticker) + '.T'
         ticker_data = Ticker(ticker_num)
 
         try:
             summary_detail = ticker_data.summary_detail[ticker_num]
-            # 最新の株価データを取得
             history = ticker_data.history(period='1d')
             if not history.empty:
-                latest_date = history.index.get_level_values('date')[-1].strftime('%Y-%m-%d')
+                latest_date = history.index.get_level_values('date')[-1].isoformat()
             else:
                 latest_date = None
 
-            # 銘柄名を取得
+           
             stock_name = df_data_j.loc[df_data_j['コード'] == ticker, '銘柄名'].values[0]
 
             stock_info = {
                 '証券コード': ticker,
-                '銘柄名': stock_name,  # 銘柄名を追加
+                '銘柄名': stock_name,
                 '株価': summary_detail.get('regularMarketPreviousClose', None),
                 '配当': summary_detail.get('dividendRate', None),
                 '配当利回り': summary_detail.get('dividendYield', None) * 100 if summary_detail.get('dividendYield') is not None else None,
                 '最新株価日付': latest_date
             }
-            print(stock_info)
+
+            headers = {
+            'Content-Type': 'application/json',
+            'x-api-key': API_KEY
+            }
+            
+            mutation = {
+                'query': '''
+                    mutation CreateStock($id: ID!, $name: String!, $price: Float, $dividend: Int, $createdAt: AWSDateTime!) {
+                    createStock(input: {id: $id, name: $name, price: $price, dividend: $dividend, createdAt: $createdAt}) {
+                        id
+                        name
+                        price
+                        dividend
+                        createdAt
+                    }
+                    }
+                ''',
+                'variables': {
+                    'id': str(uuid.uuid1()),
+                    'name': stock_info['銘柄名'],
+                    'price': stock_info['株価'],
+                    'dividend': stock_info['配当'],
+                    'createdAt': stock_info['最新株価日付']
+                }
+            }
+
+            response = requests.post(API_URL, headers=headers, data=json.dumps(mutation))
+            print(response.json())
 
         except Exception as e:
             print(f'証券コード {ticker} のデータ取得中にエラーが発生しました: {e}')
